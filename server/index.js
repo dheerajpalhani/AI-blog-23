c84b40abd967f9b8eb872da0392c812d8074c14e
dheerajpalhani <bogadheeraj09@gmail.com>
2026-08-19 18:57:48 +0530

    fix: update CORS to allow both local and vercel URLs

--- server/index.js
+++ server/index.js
@@ -18,7 +18,18 @@ connectDB();
 // CORS configuration
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        process.env.CLIENT_URL, 
+        'http://localhost:5173', 
+        'http://localhost:5003'
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
